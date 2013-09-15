e7dc5a5705d1ff83c32c8467c5e72bcbd209668f
Jesper Johansen <jesper@gundlund.com>
2013-09-15 22:52:18 +0200

    Code cleanup

--- jayj-quicktag.php
+++ jayj-quicktag.php
@@ -61,7 +61,7 @@ function jayj_quicktag_options_page() { ?>
 
 	<br />
 
-    <form action="options.php" method="post">
+	<form action="options.php" method="post">
 
 		<?php
 			/**
@@ -69,137 +69,189 @@ function jayj_quicktag_options_page() { ?>
 			 *
 			 * @since 1.1.0
 			 */
-			if ( isset( $_POST['jayj-import-quicktags-save'] ) ) :
+			if ( isset( $_POST['jayj-quicktag-import-save'] ) ) :
 
 				$options = get_option( 'jayj_qt_settings' );
-				$data = maybe_unserialize( stripslashes_deep( $_POST['jayj-import'] ) );
+				$data = maybe_unserialize( stripslashes_deep( $_POST['jayj-quicktag-import'] ) );
 
-				// Merge the old and the new Quicktags
-				if ( ! empty( $data ) )
+				// Merge the old and the new Quicktags.
+				if ( ! empty( $data ) ) {
 					$imported['buttons'] = array_merge( (array) $options['buttons'], $data['buttons'] );
+				}
 
-				// Succes or error message
-				if ( ! empty( $data ) && update_option( 'jayj_qt_settings', $imported ) )
+				// Succes or error message.
+				if ( ! empty( $data ) && update_option( 'jayj_qt_settings', $imported ) ) {
 					echo '<div class="updated"><p><strong>' . __( 'Quicktags succesfully imported', 'jayj-quicktag' ) . '</strong></p></div>';
-				else
+				} else {
 					echo '<div class="error"><p><strong>' . __( 'Error: Quicktags could not be imported', 'jayj-quicktag' ) . '</strong></p></div>';
+				}
 
 			endif;
-        ?>
-
-        <?php
-        	settings_fields( 'jayj_quicktag_options' );
-			$options = get_option( 'jayj_qt_settings' );
 		?>
 
-		<table class="widefat jayj-quicktags-table">
+		<?php
+			settings_fields( 'jayj_quicktag_options' );
+
+			/* Get the saved options. */
+			$options   = get_option( 'jayj_qt_settings' );
+			$quicktags = $options['buttons'];
+		?>
+
+		<table class="widefat jayj-quicktag-table">
+
 			<thead>
-                <tr>
-                    <th scope="col" class="jayj-order" title="<?php esc_attr_e( 'Change order', 'jayj-quicktag' ); ?>"><!-- order --></th>
-                    <th scope="col"><?php _e( 'Button Label *', 'jayj-quicktag' ); ?></th>
-                    <th scope="col"><?php _e( 'Title Attribute', 'jayj-quicktag' ); ?></th>
-                    <th scope="col"><?php _e( 'Start Tag(s) *', 'jayj-quicktag' ); ?></th>
-                    <th scope="col"><?php _e( 'End Tag(s)', 'jayj-quicktag' ); ?></th>
-                    <th scope="col" title="<?php esc_attr_e( 'Remove button', 'jayj-quicktag' ); ?>"><!-- remove --></th>
-                </tr>
-            </thead>
+				<tr>
+					<th class="jayj-quicktag-order" title="<?php esc_attr_e( 'Change order', 'jayj-quicktag' ); ?>"><!-- order --></th>
+					<th><?php _e( 'Button Label *', 'jayj-quicktag' ); ?></th>
+					<th><?php _e( 'Title Attribute', 'jayj-quicktag' ); ?></th>
+					<th><?php _e( 'Start Tag(s) *', 'jayj-quicktag' ); ?></th>
+					<th><?php _e( 'End Tag(s)', 'jayj-quicktag' ); ?></th>
+					<th title="<?php esc_attr_e( 'Remove quicktag', 'jayj-quicktag' ); ?>"><!-- remove --></th>
+				</tr>
+			</thead>
 
 			<tbody>
 				<?php
-                	if ( isset( $options['buttons'] ) ) :
+					if ( isset( $quicktags ) ) :
 
-					// Loop through all the buttons
-                	for ( $i = 0; $i < count( $options['buttons'] ); $i++ ) :
+					/* Loop through all the buttons. */
+					for ( $number = 0; $number < count( $quicktags ); $number++ ) :
 
-                		if ( ! isset( $options['buttons'][$i] ) )
-                			break;
-                ?>
-                    <tr valign="top" class="jayj-row">
-                    	<td class="jayj-order" title="<?php esc_attr_e( 'Change order', 'jayj-quicktag' ); ?>"><?php echo $i + 1; ?></td>
-                        <td><input type="text" name="jayj_qt_settings[buttons][<?php echo $i; ?>][text]" value="<?php echo esc_attr( $options['buttons'][$i]['text'] ); ?>" /></td>
-                        <td><input type="text" name="jayj_qt_settings[buttons][<?php echo $i; ?>][title]" value="<?php echo esc_attr( $options['buttons'][$i]['title'] ); ?>" /></td>
-                        <td><textarea class="code" name="jayj_qt_settings[buttons][<?php echo $i; ?>][start]" rows="2" cols="25"><?php echo esc_textarea( $options['buttons'][$i]['start'] ); ?></textarea></td>
-                        <td><textarea class="code" name="jayj_qt_settings[buttons][<?php echo $i; ?>][end]" rows="2" cols="25"><?php echo esc_textarea( $options['buttons'][$i]['end'] ); ?></textarea></td>
-                        <td class="jayj-remove"><a class="jayj-remove-button" href="javascript:;" title="<?php esc_attr_e( 'Remove button', 'jayj-quicktag' ); ?>">&times;</a></td>
-                    </tr>
-                <?php endfor; endif; ?>
+						if ( ! isset( $options['buttons'][$number] ) ) {
+							break;
+						}
+				?>
 
-                    <!-- Empty -->
-                    <?php $i = 999; ?>
-                    <tr valign="top" class="jayj-clone">
-                    	<td class="jayj-order" title="<?php esc_attr_e( 'Change order', 'jayj-quicktag' ); ?>"><?php echo $i; ?></td>
-                        <td><input type="text" name="jayj_qt_settings[buttons][<?php echo $i; ?>][text]" title="<?php esc_attr_e( 'Label of the Quicktag', 'jayj-quicktag' ); ?>" value="" /></td>
-                        <td><input type="text" name="jayj_qt_settings[buttons][<?php echo $i; ?>][title]" title="<?php esc_attr_e( 'Title attribute of the Quicktag', 'jayj-quicktag' ); ?>" value="" /></td>
-                        <td><textarea class="code" name="jayj_qt_settings[buttons][<?php echo $i; ?>][start]" rows="2" cols="25" title="<?php esc_attr_e( 'Start tag(s)', 'jayj-quicktag' ); ?>"></textarea></td>
-                        <td><textarea class="code" name="jayj_qt_settings[buttons][<?php echo $i; ?>][end]" rows="2" cols="25" title="<?php esc_attr_e( 'End tag(s)', 'jayj-quicktag' ); ?>"></textarea></td>
-						<td class="jayj-remove"><a class="jayj-remove-button" href="javascript:;" title="<?php esc_attr_e( 'Remove button', 'jayj-quicktag' ); ?>">&times;</a></td>
-                    </tr>
-    		</tbody>
-    	</table>
+					<tr valign="top" class="jayj-quicktag-row">
+						<td class="jayj-quicktag-order" title="<?php esc_attr_e( 'Change order', 'jayj-quicktag' ); ?>">
+							<?php echo $number + 1; ?>
+						</td>
 
-        <div class="jayj-table-footer">
-            <div class="jayj-order-message"></div>
-            <?php submit_button( __( 'Save Changes', 'jayj-quicktag' ) ); ?>
-            <a href="javascript:;" id="jayj-add-button" class="button-secondary"><?php _e( '+ Add New Button', 'jayj-quicktag' ); ?></a>
-        </div>
+						<td>
+							<input type="text" name="jayj_qt_settings[buttons][<?php echo intval( $number ); ?>][text]" value="<?php echo esc_attr( $quicktags[$number]['text'] ); ?>" />
+						</td>
+
+						<td>
+							<input type="text" name="jayj_qt_settings[buttons][<?php echo intval( $number ); ?>][title]" value="<?php echo esc_attr( $quicktags[$number]['title'] ); ?>" />
+						</td>
+
+						<td>
+							<textarea class="code" name="jayj_qt_settings[buttons][<?php echo intval( $number ); ?>][start]" rows="2" cols="25"><?php echo esc_textarea( $quicktags[$number]['start'] ); ?></textarea>
+						</td>
+
+						<td>
+							<textarea class="code" name="jayj_qt_settings[buttons][<?php echo intval( $number ); ?>][end]" rows="2" cols="25"><?php echo esc_textarea( $quicktags[$number]['end'] ); ?></textarea>
+						</td>
+
+						<td class="jayj-quicktag-remove">
+							<button class="button jayj-quicktag-remove-button" title="<?php esc_attr_e( 'Remove quicktag', 'jayj-quicktag' ); ?>">&times;</button>
+						</td>
+					</tr>
+
+				<?php endfor; endif; ?>
+
+					<!-- Empty row -->
+					<?php $i = 9999; ?>
+
+                    <tr valign="top" class="jayj-quicktag-clone <?php if ( empty ( $options['buttons'] ) ) echo 'jayj-quicktag-clone-show'; ?>">
+                    	<td class="jayj-quicktag-order" title="<?php esc_attr_e( 'Change order', 'jayj-quicktag' ); ?>"><?php echo $i; ?></td>
+
+                    	<?php
+                    		/* Set the name attribute. */
+                    		$clone_name = 'jayj_qt_settings[buttons][' . $i . ']';
+                    	?>
+
+                        <td>
+                        	<input type="text" name="<?php echo esc_attr( $clone_name ); ?>[text]" title="<?php esc_attr_e( 'Label of the Quicktag', 'jayj-quicktag' ); ?>" placeholder="<?php esc_attr_e( 'Example', 'jayj-quicktag' ); ?>" value="" />
+                        </td>
+
+                        <td>
+                        	<input type="text" name="<?php echo esc_attr( $clone_name ); ?>[title]" title="<?php esc_attr_e( 'Title attribute of the Quicktag', 'jayj-quicktag' ); ?>" placeholder="<?php esc_attr_e( 'Example title', 'jayj-quicktag' ); ?>" value="" />
+                        </td>
+
+                        <td>
+                        	<textarea class="code" name="<?php echo esc_attr( $clone_name ); ?>[start]" rows="2" cols="25" title="<?php esc_attr_e( 'Start tag(s)', 'jayj-quicktag' ); ?>" placeholder="<?php esc_attr_e( '<example>', 'cakifo'); ?>"></textarea>
+                        </td>
+
+                        <td>
+                        	<textarea class="code" name="<?php echo esc_attr( $clone_name ); ?>[end]" rows="2" cols="25" title="<?php esc_attr_e( 'Optional: End tag(s)', 'jayj-quicktag' ); ?>" placeholder="<?php esc_attr_e( '</example>', 'jayj-quicktag' ); ?>"></textarea>
+                        </td>
+
+						<td class="jayj-quicktag-remove">
+							<button class="button jayj-quicktag-remove-button" title="<?php esc_attr_e( 'Remove button', 'jayj-quicktag' ); ?>">&times;</button>
+						</td>
+                    </tr> <!-- .jayj-quicktag-clone -->
+			</tbody>
+		</table>
+
+		<div class="jayj-quicktag-table-footer">
+			<?php submit_button( __( 'Save Changes', 'jayj-quicktag' ), 'primary', 'submit', false ); ?>
+
+			<button class="jayj-quicktag-add-quicktag button-secondary"><?php _e( '+ Add New Button', 'jayj-quicktag' ); ?></button>
+		</div>
 	</form>
 
 	<!-- Export/Import metaboxes -->
-    <form action="" method="post" name="jayj-import-quicktags">
-	<div id="poststuff"><div class="metabox-holder">
+	<form action="" method="post" name="jayj-quicktag-import" class="jayj-quicktag-postboxes">
+
+		<div id="poststuff"><div class="metabox-holder">
 
 		<!-- Export function -->
-        <div class="postbox closed jayj-quicktags-postbox">
-        	<div class="handlediv" title="<?php esc_attr_e( 'Click to toggle', 'jayj-quicktag' ); ?>"><br /></div>
-            <h3 class="hndle"><span><?php _e( 'Export Quicktags', 'jayj-quicktag' ); ?></span></h3>
+		<div class="postbox closed">
+			<div class="handlediv" title="<?php esc_attr_e( 'Click to toggle', 'jayj-quicktag' ); ?>"><br /></div>
 
-            <div class="inside">
-				<?php _e( 'Export your saved Quicktags data by highlighting this text and either', 'jayj-quicktag' ); ?>
-                <ul>
-                    <li><?php _e( 'Copy/paste it into a blank .txt file. Then save the file for importing into another install of WordPress later.', 'jayj-quicktag' ); ?></li>
-                    <li><?php _e( 'Or you could just paste it into <code>Jayj Quicktag > Import Quicktags</code> on another install of WordPress.', 'jayj-quicktag' ); ?></li>
-                </ul>
-            	<textarea rows="10" cols="60" onclick="this.focus(); this.select();"><?php echo esc_textarea( serialize( $options ) ); ?></textarea>
-            </div>
-        </div> <!-- .postbox -->
+			<h3 class="hndle"><span><?php _e( 'Export Quicktags', 'jayj-quicktag' ); ?></span></h3>
+
+			<div class="inside">
+				<?php _e( 'Export your saved Quicktags data by highlighting the following text, and either:', 'jayj-quicktag' ); ?>
+				<ul>
+					<li><?php _e( 'Copy/paste it into a blank .txt file. Then save the file for importing into another install of WordPress later.', 'jayj-quicktag' ); ?></li>
+					<li><?php _e( 'Or you could paste it into <code>Jayj Quicktag > Import Quicktags</code> on another WordPress install.', 'jayj-quicktag' ); ?></li>
+				</ul>
+				<textarea rows="10" cols="60" onclick="this.focus(); this.select();"><?php echo esc_textarea( serialize( $options ) ); ?></textarea>
+			</div>
+		</div> <!-- .postbox -->
 
 		<!-- Import function -->
-        <div class="postbox closed jayj-quicktags-postbox jayj-quicktags-postbox-last">
-        	<div class="handlediv" title="<?php esc_attr_e( 'Click to toggle', 'jayj-quicktag' ); ?>"><br /></div>
-            <h3 class="hndle"><span><?php _e( 'Import Quicktags', 'jayj-quicktag' ); ?></span></h3>
+		<div class="postbox closed">
+			<div class="handlediv" title="<?php esc_attr_e( 'Click to toggle', 'jayj-quicktag' ); ?>"><br /></div>
 
-            <div class="inside">
-                <p><?php _e( 'To import your Quicktags, copy and paste the content from "Export Quicktags" into this textarea and press the "Import Quicktags" button below.', 'jayj-quicktag' ); ?></p>
-                <textarea rows="10" cols="60" name="jayj-import"></textarea>
-                <?php submit_button( __( 'Import Quicktags', 'jayj-quicktag' ), 'secondary', 'jayj-import-quicktags-save' ); ?>
-            </div>
-        </div> <!-- .postbox -->
+			<h3 class="hndle"><span><?php _e( 'Import Quicktags', 'jayj-quicktag' ); ?></span></h3>
 
-    </div></div>
+			<div class="inside">
+				<p><?php _e( 'To import your Quicktags, copy and paste the content from "Export Quicktags" into this textarea and press the "Import Quicktags" button below.', 'jayj-quicktag' ); ?></p>
+				<textarea rows="10" cols="60" name="jayj-quicktag-import"></textarea>
+				<?php submit_button( __( 'Import Quicktags', 'jayj-quicktag' ), 'secondary', 'jayj-quicktag-import-save' ); ?>
+			</div>
+		</div> <!-- .postbox -->
+
+		</div></div>
 	</form>
 
 </div> <!-- .wrap -->
 
 <?php
 
-} // End function wpaq_options_page
+} // End jayj_quicktag_options_page()
+
 
 /**
- * Validate the saved options
+ * Validate the saved quicktags.
  *
- * This will make sure that options with an empty "Button Label" value will be removed
+ * This will make sure removed quicktags or quicktags with an
+ * empty "Button label" will be removed.
  *
- * @since 1.0.0
- * @param array $input The options saved on the options page
- * @returns array The options saved, without the empty ones
+ * @param  array $input The quicktags saved on the options page
+ * @return array        The quicktags with the empty ones removed
  */
 function jayj_quicktag_options_validate( $input ) {
 
-	// Don't save empty inputs
+	// Don't save empty inputs.
 	foreach( $input['buttons'] as $i => $btn ) :
 
-		if ( empty( $btn['text'] ) )
+		if ( empty( $btn['text'] ) ) {
 			unset( $input['buttons'][$i], $btn );
+		}
 
 	endforeach;
 
@@ -245,7 +297,7 @@ function jayj_quicktag_editor() {
 	if ( ! did_action( 'before_wp_tiny_mce' ) )
 		return;
 
-	// Get the options
+	// Get the options.
 	$options = get_option( 'jayj_qt_settings' );
 
 	if ( count( $options['buttons'] ) > 0 ) : ?>
@@ -253,9 +305,9 @@ function jayj_quicktag_editor() {
 		<!-- Jayj Quicktags -->
 		<script type="text/javascript">
 		//<![CDATA[
-			if (typeof(QTags) != 'undefined') {
+			if ( typeof(QTags) != 'undefined' ) {
 				<?php
-					$i = 0;
+					$number = 0;
 
 					// Loop through each button
 					foreach ( $options['buttons'] as $btn ) :
@@ -264,9 +316,10 @@ function jayj_quicktag_editor() {
 				?>
 
 					<?php // Self-closing tag ?>
+
 					<?php if ( empty( $btn['end'] ) ) { ?>
 						QTags.addButton(
-							'jayj_qtag_<?php echo intval( $i ); ?>',
+							'jayj_qtag_<?php echo intval( $number ); ?>',
 							'<?php echo esc_attr( $btn['text'] ); ?>',
 							'<?php echo addslashes( $btn['start'] ); ?>',
 							'', '',
@@ -274,7 +327,7 @@ function jayj_quicktag_editor() {
 						);
 					<?php } else { ?>
 						QTags.addButton(
-							'jayj_qtag_<?php echo intval( $i ); ?>',
+							'jayj_qtag_<?php echo intval( $number ); ?>',
 							'<?php echo esc_attr( $btn['text'] ); ?>',
 							'<?php echo addslashes( $btn['start'] ); ?>',
 							'<?php echo addslashes( $btn['end'] ); ?>',
@@ -282,7 +335,9 @@ function jayj_quicktag_editor() {
 							'<?php echo esc_attr( $title ); ?>'
 						);
 					<?php } // endif
-						$i++;
+
+						$number++;
+
 					endforeach;
 				?>
 			}
